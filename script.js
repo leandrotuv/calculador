c7237967f12c8ea2f520ec01fdfd9b03c8680970
leandrotuv <150416884+leandrotuv@users.noreply.github.com>
2024-02-13 14:12:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculateResult() {
+    let expression = document.getElementById('display').value;
+    let result = eval(expression);
+    document.getElementById('display').value = result;
+  }
